6b5ddc5d9b79e22f086c7296bf0e8c45426c7f9d
ariannacostanzo <costanzo.arianna@outlook.it>
2024-03-01 15:28:09 +0100

    fiished 4. query group

--- db-university/solutions.sql
+++ db-university/solutions.sql
@@ -89,3 +89,6 @@ GROUP BY `exam_id`;
 
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
 
+SELECT COUNT(*) AS `total_courses`, `department_id`
+FROM `degrees`
+GROUP BY `department_id`;
