d82c20b864aeddc4f3176ddf219a82d5bd8e6904
ariannacostanzo <costanzo.arianna@outlook.it>
2024-03-01 15:19:49 +0100

    finished 2. query group

--- db-university/solutions.sql
+++ db-university/solutions.sql
@@ -67,6 +67,10 @@ GROUP BY `year`;
 
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
 
+SELECT COUNT(*) AS `total_teachers`, `office_address`
+FROM `teachers`
+GROUP BY `office_address`;
+
 
 -- 3. Calcolare la media dei voti di ogni appello d'esame
 
